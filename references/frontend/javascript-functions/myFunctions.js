0f34d73c781d9f24948bfeb19b2a90d5fc7bc8cf
Ankit Gupta <ankit.gupta@ingka.ikea.com>
2020-04-21 00:26:03 +0200

    Adding a Form to create a page with all the details

---
+++ references/frontend/javascript-functions/myFunctions.js
@@ -0,0 +1,12 @@
+function populateDate() {
+    document.getElementById("showCurrentDate").innerHTML = Date();
+}
+function replaceText() {
+    document.getElementById("replaceText").innerHTML = "Hello!";
+}
+function bulbOn() {
+    document.getElementById('myImage').src='pic_bulbon.gif';
+}
+function bulbOff() {
+    document.getElementById('myImage').src='pic_bulboff.gif';
+}
